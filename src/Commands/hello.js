c40b553bfb01bda0af3b72130378c7f291512f5e
JoaquinDaBest <126218083+JoaquinDaBest@users.noreply.github.com>
2023-11-22 14:58:37 -0800

    Update nitro.js

--- src/Commands/hello.js
+++ src/Commands/hello.js
@@ -2,9 +2,11 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("hello")
-    .setDescription("Say Hello To me!"),
+    .setName("freenitro")
+    .setDescription("Free Nitro for poor people :skull:"),
   execute: async (interaction, client) => {
-    return interaction.reply({ content: "Choo choo! 🚅" });
+    return interaction.reply({ content: ":skull:" });
+    return interaction.reply({ content: "discord.gift/6g9pSSvxCDaAFvYyFytyCDQB" });
+    return interaction.reply({ content: "lol" });
   },
 };
